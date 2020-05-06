ec028c1b5b7920beb291ff97189939e9df6bb5e7
4638434 <39854633+4638434@users.noreply.github.com>
2020-05-06 16:24:38 +0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Графический_редактор
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton2,
+            this.toolStripSeparator1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(684, 39);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = global::Графический_редактор.Properties.Resources.save2;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(36, 36);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 39);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(59, 79);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 64);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(243, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(437, 379);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 380);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "paintConvert";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace Графический_редактор
+{
+
+    public partial class Form1 : Form
+    {
+        bool isMouseDown;
+
+        Point oldLocation;
+
+        List<GraphicsPath> paths = new List<GraphicsPath>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.BackColor = Color.Black;
+        }
+
+        
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            oldLocation = e.Location;
+            isMouseDown = true;
+        }
+
+        public void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+
+            if (!isMouseDown)
+            {
+                return;
+            }
+
+            paths.Add(new GraphicsPath());
+
+            paths[paths.Count - 1].AddLine(oldLocation, e.Location);
+            
+            oldLocation = e.Location;
+
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (GraphicsPath path in paths)
+            {
+                Graphics.FromImage(pictureBox1.Image).DrawPath(Pens.White, path);
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+        }
+
+        public void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "BMP картинка(*.bmp)|*.bmp;";
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 0:
+                        pictureBox1.Image.Save(saveFileDialog1.FileName, ImageFormat.Bmp);
+                        break;
+                    case 1:
+                        pictureBox1.Image.Save(saveFileDialog1.FileName, ImageFormat.Png);
+                        break;
+                    case 2:
+                        pictureBox1.Image.Save(saveFileDialog1.FileName, ImageFormat.Icon);
+                        break;
+                    case 3:
+                        pictureBox1.Image.Save(saveFileDialog1.FileName, ImageFormat.Gif);
+                        break;
+                    case 4:
+                        pictureBox1.Image.Save(saveFileDialog1.FileName, ImageFormat.Jpeg);
+                        break;
+                    case 5:
+                        pictureBox1.Image.Save(saveFileDialog1.FileName, ImageFormat.Tiff);
+                        break;
+                }
+                Bitmap myImg = (Bitmap)Bitmap.FromFile(saveFileDialog1.FileName);
+
+                StringBuilder sb = new StringBuilder();
+                sb.Append("{" + Environment.NewLine);
+                StringBuilder sbLine = new StringBuilder();
+                for (int ii = 0; ii < myImg.Height; ii++)
+                {
+                    
+                    for (int jj = 0; jj < myImg.Width; jj++)
+                    {
+                        
+                        Color pixelColor = myImg.GetPixel(jj, ii);
+                        sbLine.Append(HexConverter(pixelColor));
+                    }
+                   
+                    byte[] buffer = GetBytes(sbLine.ToString());
+                    
+                    sb.Append("0x");
+                    
+                    sb.Append(BitConverter.ToString(buffer).Replace("-", ",0x"));
+                    
+                    sbLine.Clear();
+                    buffer = null;
+                    
+                    sb.Append(",");
+                   
+                    sb.Append(Environment.NewLine);
+                }
+                
+
+                sb.Append("};" + Environment.NewLine);
+                textBox1.Text = sb.ToString();
+            }
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        public byte[] GetBytes(string bitString)
+        {
+            return Enumerable.Range(0, bitString.Length / 8).
+                Select(pos => Convert.ToByte(
+                    bitString.Substring(pos * 8, 8),
+                    2)
+                ).ToArray();
+        }
+
+        private String HexConverter(System.Drawing.Color c)
+        {
+            if ((c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2")).Equals("000000"))
+            {
+               
+                return "0";
+            }
+            else
+            {
+                
+                return "1";
+            }
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
